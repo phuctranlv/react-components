a8631f6ddc1c79477f72abcd900f9ff16eb68644
Phuc Tran <phuctranlv@gmail.com>
2020-02-18 18:19:20 -0800

    Complete the React Components excercise

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    }
+  }
+  onGroceryItemClick() {
+    this.setState({
+      done: !this.state.done
+    })
+  }
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+    return (<li style={style} onClick={this.onGroceryItemClick.bind(this)}>{this.props.item}</li>)
+  }
+}
+
+
+var GroceryList = (props) => (
+<ul>
+    {props.items.map(item => 
+    <GroceryListItem item={item}/>
+    )}
+</ul>
+)
+  
+
+
+ReactDOM.render(<GroceryList items={['Breakfast sandwich', 'milk']}/>, document.getElementById("app"));
+
